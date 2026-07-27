8e64bc145c53e3ae5593f3ba3838d137f9c2ff12
artiehumphreys <artieh2003@gmail.com>
2026-07-27 12:22:57 -0500

    feat(1brc): global state class for managing data + prefer span w/ iterators over byte arithmetic

--- 1brc.cpp
+++ 1brc.cpp
@@ -1,22 +1,57 @@
 #include <algorithm>
 #include <cassert>
 #include <cstddef>
+#include <cstdint>
 #include <cstdio>
 #include <filesystem>
+#include <iterator>
+#include <limits>
+#include <mutex>
+#include <span>
+#include <string_view>
 #include <sys/mman.h>
 #include <thread>
+#include <unordered_map>
 #include <vector>
 
 const char *FILE_NAME = "input.txt";
 
 namespace fs = std::filesystem;
 
-std::size_t next_line_start(const unsigned char *data, std::size_t end,
-                            std::size_t size) {
-  while (end < size && data[end] != '\n') {
-    ++end;
+struct Stats {
+  // store sum, count, min, max
+  float sum{0};
+  std::uint32_t count = 0;
+  float min = std::numeric_limits<float>::max();
+  float max = std::numeric_limits<float>::max();
+};
+
+class GlobalState {
+public:
+  GlobalState() = default;
+
+  void add_measurement(std::string_view station, float val) {
+    std::lock_guard<std::mutex> lk(mtx_);
+    Stats &s = mp_[station];
+
+    s.sum += val;
+    ++s.count;
+    s.min = std::min(s.min, val);
+    s.max = std::max(s.max, val);
   }
-  return end < size ? end + 1 : size;
+
+private:
+  std::mutex mtx_;
+  std::unordered_map<std::string_view, Stats> mp_;
+};
+
+GlobalState gs{};
+
+std::size_t next_line_start(std::span<const char> data, std::size_t from) {
+  // invariant: every line ends in newline character
+  const auto newline =
+      std::ranges::find(data.begin() + from, std::unreachable_sentinel, '\n');
+  return newline == data.end() ? data.size() : ((newline - data.begin()) + 1);
 }
 
 int main() {
@@ -42,21 +77,25 @@ int main() {
     return 1;
   }
 
-  const unsigned char *chr = static_cast<const unsigned char *>(f);
+  const char *chr = static_cast<const char *>(f);
 
   const std::size_t num_threads =
       std::max(1u, std::thread::hardware_concurrency());
   const std::size_t estimated_chunk_size = size / num_threads;
 
+  std::span<const char> data(chr, size);
+
   std::size_t begin = 0;
+
   for (std::size_t i = 0; i < num_threads && begin < size; ++i) {
     // statically calculate the bounds of each worker thread
     std::size_t end =
         (i + 1 == num_threads)
             ? size
-            : next_line_start(chr, std::min(begin + estimated_chunk_size, size),
-                              size);
-    // worker_threads.emplace_back(process, chr, begin, end); // [begin, end)
+            : next_line_start(data,
+                              std::min(begin + estimated_chunk_size, size));
+    worker_threads.emplace_back(
+        process, data.subspan(begin, end - begin)); // [begin, end)
     begin = end;
   }
 
